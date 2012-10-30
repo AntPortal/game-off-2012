74cfc64b9a308db42a5ad986c9c68458f68c5c63
Nebu Pookins <nebu@nebupookins.net>
2012-10-30 12:54:18 -0400

    Improved Crafty-Tiled map loader
    * Supports maps with multiple tilesets.
    * Supports "baseheight" property in layer.
    * Autocorrects image filepath.
    * Doesn't draw "tile0", which fixes tinting.

--- app/scenes/IsoTest.js
+++ app/scenes/IsoTest.js
@@ -1,12 +1,9 @@
-define([ 'config', 'maps/test.json', 'Crafty' ], function(config, mapData) {
-
-	
+define([ 'config', 'maps/test-multi-tileset-two-baseheights.json', 'Crafty' ], function(config, mapData) {
 	Crafty.scene('IsoTest', function() {
-		var tileX, tileY, pixelX, pixleY, tileType, i, j;
+		var tileX, tileY, pixelX, pixelY, tileType, i, j;
 		//Load tileset into crafty
-		//TODO: Only support 1 tileset for now
 		for (i =0; i < mapData.tilesets.length; i++) {
-			var tileset = mapData.tilesets[i]
+			var tileset = mapData.tilesets[i];
 			//TODO: margin and spacing are ignored.
 			var tileId = tileset.firstgid;
 			var craftySpriteData = {};
@@ -18,8 +15,14 @@ define([ 'config', 'maps/test.json', 'Crafty' ], function(config, mapData) {
 					tileId++;
 				}
 			}
+			/*
+			 * Tiled saves images relative to the map, which is in /assets/maps. Crafty wants images
+			 * relative to /. The images are in /assets/tiles, so basically we replace the ".." with "assets"
+			 * to perform the conversion, e.g. "../tiles/tileset.png" -> "assets/tiles/tileset.png";
+			 */
+			var fixedPath = "assets" + tileset.image.substr(2);
 			//TODO: fix image path
-			Crafty.sprite(tileset.tileheight, tileset.tilewidth, tileset.image, craftySpriteData);
+			Crafty.sprite(tileset.tileheight, tileset.tilewidth, fixedPath, craftySpriteData);
 		}
 		
 		//Render map
@@ -27,27 +30,28 @@ define([ 'config', 'maps/test.json', 'Crafty' ], function(config, mapData) {
 			var layer = mapData.layers[i];
 			if (layer.visible) {
 				for (j = 0; j < layer.data.length; j++) {
-					tileType = 'tile'+layer.data[j];
-					tileX = j % layer.width;
-					tileY = Math.floor(j / layer.width);
-					pixelX = (config.viewport.width / 2) + ((tileX - tileY) * mapData.tilewidth / 2);
-					pixelY = ((tileX + tileY) * mapData.tileheight / 2);
-					console.log({tileX: tileX,tileY: tileY,pixelX:pixelX,pixelY:pixelY});
-					var entity = Crafty.e('2D, Canvas, Mouse, ' + tileType + ', Tint').attr({
-						w : 64,
-						h : 64,
-						x: pixelX,
-						y: pixelY,
-						tileX: tileX,
-						tileY: tileY
-					});
-					entity.bind('MouseOver', function(event) {
-						this.tint("#0000FF", 0.25);
-						console.log({tileX: this.tileX, tileY: this.tileY});
-					});
-					entity.bind('MouseOut', function(event) {
-						this.tint("#000000", 0);
-					});
+					if (layer.data[j] != 0) {
+						tileType = 'tile'+layer.data[j];
+						tileX = j % layer.width;
+						tileY = Math.floor(j / layer.width);
+						pixelX = (config.viewport.width / 2) + ((tileX - tileY) * mapData.tilewidth / 2);
+						pixelY = ((tileX + tileY) * mapData.tileheight / 2) - (layer.properties.baseheight * mapData.tileheight); 
+						var entity = Crafty.e('2D, Canvas, Mouse, ' + tileType + ', Tint').attr({
+							w : 64,
+							h : 64,
+							x: pixelX,
+							y: pixelY,
+							tileX: tileX,
+							tileY: tileY
+						});
+						entity.bind('MouseOver', function(event) {
+							this.tint("#0000FF", 0.25);
+							console.log({tileX: this.tileX, tileY: this.tileY});
+						});
+						entity.bind('MouseOut', function(event) {
+							this.tint("#000000", 0);
+						});
+					}
 				}
 			}
 		} 
