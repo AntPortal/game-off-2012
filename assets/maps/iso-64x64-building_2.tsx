173216530208ec174ade15a027e36e764c569df0
Nebu Pookins <nebu@nebupookins.net>
2012-10-31 15:50:30 -0400

    Committed the two exported tilesets.

---
+++ assets/maps/iso-64x64-building_2.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="iso-64x64-building_2" tilewidth="64" tileheight="64">
+ <image source="../tiles/iso-64x64-building_2.png" width="640" height="512"/>
+ <tile id="54">
+  <properties>
+   <property name="heightoffset" value="0.5"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ assets/maps/iso-64x64-outside.tsx
@@ -0,0 +1,120 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="iso-64x64-outside" tilewidth="64" tileheight="64">
+ <image source="../tiles/iso-64x64-outside.png" width="640" height="1024"/>
+ <tile id="31">
+  <properties>
+   <property name="heightoffset" value="0.5"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="heightoffset" value="0.5"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="areaMap" value="cube"/>
+   <property name="heightoffset" value="1"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="heightoffset" value="1"/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="noStand" value="noStand"/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="121">
+  <properties>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="128">
+  <properties>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="129">
+  <properties>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="132">
+  <properties>
+   <property name="addUp" value="1"/>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="133">
+  <properties>
+   <property name="addUp" value="1"/>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="150">
+  <properties>
+   <property name="addUp" value="2"/>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="151">
+  <properties>
+   <property name="addUp" value="2"/>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="152">
+  <properties>
+   <property name="addUp" value="1"/>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="153">
+  <properties>
+   <property name="addUp" value="1"/>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="155">
+  <properties>
+   <property name="addSides" value="1"/>
+   <property name="addUp" value="1"/>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+ <tile id="158">
+  <properties>
+   <property name="addSides" value="1"/>
+   <property name="addUp" value="2"/>
+   <property name="noStand" value="true"/>
+  </properties>
+ </tile>
+</tileset>
