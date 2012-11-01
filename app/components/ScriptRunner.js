1c6ad56038e43e8bc9b13f0ebc52249f41f77b7d
Nebu Pookins <nebu@nebupookins.net>
2012-11-01 14:53:27 -0400

    Added prototype script runner.

---
+++ app/components/ScriptRunner.js
@@ -0,0 +1,55 @@
+define([
+	'config',
+	'Crafty'
+], function(config) {
+	Crafty.c('ScriptRunner', {
+		_curState: 0,
+		init: function() {
+			//Does nothing
+			return this;
+		},
+		ScriptRunner: function(script) {
+			this.script = script;
+			return this;
+		},
+		run: function() {
+			if (this._curState >= this.script.length) {
+				return; //Done
+			}
+			var instruction = this.script[this._curState];
+			switch (instruction.action) {
+			case "dialog":
+				this._dialog(instruction);
+				break;
+			case "PACADOC": //Pause and close all dialogs on click
+				this._PACADOC(instruction);
+				break;
+			default:
+				throw "Unsupported scripting action: " + instruction.action;
+			}
+			return this;
+		},
+		_dialog: function(dialogInst) {
+			if (dialogInst.action != "dialog") {
+				throw dialogInst;
+			}
+			Crafty.e('2D, Canvas, Dialog').Dialog(dialogInst.params);
+			this._curState++;
+			this.run();
+		},
+		_PACADOC: function(pacadocInst) {
+			if (pacadocInst.action != "PACADOC") {
+				throw pacadocInst;
+			}
+			var me = this;
+			Crafty.e('2D, Mouse').attr({
+				x: 0, y: 0, w: config.viewport.width, h: config.viewport.height, clicked: false
+			}).bind('Click', function() {
+				Crafty('Dialog').destroy(); //Destroy all dialogs
+				this.destroy(); //Destroy this mouse listener
+				me._curState++;
+				me.run();
+			});
+		}
+	});
+});
--- app/scenes/level1-intro.js
+++ app/scenes/level1-intro.js
@@ -6,6 +6,7 @@ define([
 		'components/ViewportRelative',
 		'components/Character',
 		'components/Dialog',
+		'components/ScriptRunner',
 	], function(config, mapData, utils) {
 	Crafty.scene('level1-intro', function() {
 		var tileProperties = utils.loadTileset(mapData);
@@ -13,37 +14,61 @@ define([
 			//Does nothing
 		});
 		//Add characters
-		var mouseListener = Crafty.e('2D, Mouse').attr({
-			x: 0, y: 0, w: config.viewport.width, h: config.viewport.height, clicked: false
-		}).bind('Click', function() {
-			this.clicked = true;
-		});
 		var worldToPixel = utils.makeWorldToPixelConverter(mapData.tilewidth, mapData.tileheight);
 		var hero = Crafty.e('2D, Canvas, Character, heroSouth').Character(heightMap, worldToPixel, 3, 0);
 		var mom = Crafty.e('2D, Canvas, Character, momSouth').Character(heightMap, worldToPixel, 1, 0);
-		(function() {
-			//Handle HUD
-			utils.addMusicControlEntity(Crafty);
-			var dialog1 = Crafty.e('2D, Canvas, Dialog').Dialog({
-				x: 150,
-				y: 100,
-				w: 320,
-				h: 70,
-				msg: "Mom: [Name], wake up!",
-				showMore: true,
-				face: 'face_villagerF'
-			});
-			var dialog2 = Crafty.e('2D, Canvas, Dialog').Dialog({
-				x: 150,
-				y: 100,
-				w: 320,
-				h: 70,
-				msg: "[Name]: Mfhg... Five more minutes...",
-				showMore: true,
-				face: 'face_warriorM'
-			});
-			dialog2.visible = false;
-		})();
+		var script = [
+			{
+				action: "dialog",
+				params: {
+					x: 150,
+					y: 100,
+					w: 320,
+					h: 70,
+					msg: "[Name]...",
+					showMore: true,
+				}
+			},
+			{ action: "PACADOC" },
+			{
+				action: "dialog",
+				params: {
+					x: 150,
+					y: 100,
+					w: 320,
+					h: 70,
+					msg: "[Name]!",
+					showMore: true,
+				}
+			},
+			{ action: "PACADOC" },
+			{
+				action: "dialog",
+				params: {
+					x: 150,
+					y: 100,
+					w: 320,
+					h: 70,
+					msg: "Good morning, [Name]!",
+					showMore: true,
+				}
+			},
+			{ action: "PACADOC" },
+			{
+				action: "dialog",
+				params: {
+					x: 150,
+					y: 100,
+					w: 320,
+					h: 70,
+					msg: "Mom: Come on, sleepy head! Get up!",
+					showMore: true,
+					face: 'face_villagerF',
+				}
+			},
+			{ action: "PACADOC" },
+		];
+		Crafty.e('2D, ScriptRunner').ScriptRunner(script).run();
 		Crafty.viewport.clampToEntities = false;
 		Crafty.viewport.y = config.viewport.height / 2;
 		Crafty.audio.play('music/town', -1, utils.effectiveVolume('music/town'));
