1329e893bb348b865992dee368fc890ed4c91223
Nebu Pookins <nebu@nebupookins.net>
2012-11-01 18:12:00 -0400

    Added fix to ensure Google Web Fonts is always loaded before game starts.

--- app/scenes/Loading.js
+++ app/scenes/Loading.js
@@ -30,6 +30,16 @@ define([
 			var mp3 = config.music[key].prefix + '.mp3';
 			Crafty.audio.add(key, [ ogg, mp3 ]);
 		}
+		function makeSleepUntilFontsLoaded(doneCallback) {
+			var func = function() {
+				if (fontsLoaded) {
+					doneCallback();
+				} else {
+					window.setTimeout(func, 500);
+				}
+			};
+			return func;
+		}
 		Crafty.load([ //Try to keep alphabetical
 				'assets/faces/faces1.png',
 				'assets/sprites/charsets_warrior.png',
@@ -39,32 +49,34 @@ define([
 				'assets/ui/comment_new.gif', 'assets/ui/dialog.blue.png',
 				'assets/ui/music.png',
 		], function() {
-			// TODO load other assets
-			loadAntifareaCharacterSprite('hero', 'assets/sprites/charsets_warrior.png');
-			loadAntifareaCharacterSprite('mom', 'assets/sprites/mom.png');
-			// Load dialog box
-			Crafty.sprite(16, 'assets/ui/dialog.blue.png', {
-				dialog7 : [ 0, 0 ], dialog8 : [ 1, 0 ], dialog9 : [ 2, 0 ],
-				dialog4 : [ 0, 1 ], dialog5 : [ 1, 1 ], dialog6 : [ 2, 1 ],
-				dialog1 : [ 0, 2 ], dialog2 : [ 1, 2 ], dialog3 : [ 2, 2 ],
-			});
-			Crafty.sprite(16, 'assets/ui/comment_new.gif', {
-				dialogMore : [ 0, 0 ]
-			});
-			(function() {
-				//Loads the faces as face_warriorM, face_warriorF, face_mageM, etc.
-				var temp = ['warrior', 'mage', 'healer', 'ninja', 'ranger', 'villager'];
-				var i;
-				var spriteParam = {};
-				for (i = 0; i < temp.length; i++) {
-					spriteParam['face_'+temp[i]+'M'] = [i, 0];
-					spriteParam['face_'+temp[i]+'F'] = [i, 1];
-				}
-				Crafty.sprite(48, 'assets/faces/faces1.png', spriteParam);
+			makeSleepUntilFontsLoaded(function() {
+				// TODO load other assets
+				loadAntifareaCharacterSprite('hero', 'assets/sprites/charsets_warrior.png');
+				loadAntifareaCharacterSprite('mom', 'assets/sprites/mom.png');
+				// Load dialog box
+				Crafty.sprite(16, 'assets/ui/dialog.blue.png', {
+					dialog7 : [ 0, 0 ], dialog8 : [ 1, 0 ], dialog9 : [ 2, 0 ],
+					dialog4 : [ 0, 1 ], dialog5 : [ 1, 1 ], dialog6 : [ 2, 1 ],
+					dialog1 : [ 0, 2 ], dialog2 : [ 1, 2 ], dialog3 : [ 2, 2 ],
+				});
+				Crafty.sprite(16, 'assets/ui/comment_new.gif', {
+					dialogMore : [ 0, 0 ]
+				});
+				(function() {
+					//Loads the faces as face_warriorM, face_warriorF, face_mageM, etc.
+					var temp = ['warrior', 'mage', 'healer', 'ninja', 'ranger', 'villager'];
+					var i;
+					var spriteParam = {};
+					for (i = 0; i < temp.length; i++) {
+						spriteParam['face_'+temp[i]+'M'] = [i, 0];
+						spriteParam['face_'+temp[i]+'F'] = [i, 1];
+					}
+					Crafty.sprite(48, 'assets/faces/faces1.png', spriteParam);
+				})();
+				// When done loading, transition to Title scene.
+				// Crafty.scene('Title');
+				Crafty.scene('level1-intro'); //TODO
 			})();
-			// When done loading, transition to Title scene.
-			// Crafty.scene('Title');
-			Crafty.scene('level1-intro'); //TODO
 		});
 	});
 	return undefined;
