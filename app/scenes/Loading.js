41b0f79e856e1ad5551c0209d0cfeac39e073071
Nebu Pookins <nebu@nebupookins.net>
2012-11-03 02:10:58 -0400

    Fixed comments.

--- app/scenes/Loading.js
+++ app/scenes/Loading.js
@@ -55,6 +55,7 @@ define([
 				'assets/ui/dialog.blue.png',
 				'assets/ui/music.png',
 			];
+			//Adds Github avatars to the list of assets to load.
 			var uniqueGithubAvatarUrls = utils.removeDuplicates(githubAvatarUrls);
 			for (i = 0; i < uniqueGithubAvatarUrls.length; i++) {
 				var url = uniqueGithubAvatarUrls[i];
@@ -64,13 +65,12 @@ define([
 			}
 			Crafty.load(assets, function() {
 				makeSleepUntilFontsLoaded(function() {
-					// TODO load other assets
 					loadAntifareaCharacterSprite('hero', 'assets/sprites/charsets_warrior.png');
 					loadAntifareaCharacterSprite('mom', 'assets/sprites/mom.png');
-					// Load dialog box
 					Crafty.sprite(917, 'assets/ui/bg-blue.png', {
 						ui_bg_blue: [0, 0]
 					});
+				// Create sprite for dialog box
 					Crafty.sprite(16, 'assets/ui/dialog.blue.png', {
 						dialog7 : [ 0, 0 ], dialog8 : [ 1, 0 ], dialog9 : [ 2, 0 ],
 						dialog4 : [ 0, 1 ], dialog5 : [ 1, 1 ], dialog6 : [ 2, 1 ],
