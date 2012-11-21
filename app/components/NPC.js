dbf22e9d38a74675ce36301e955db36bc16d8e4d
Nebu Pookins <nebu@nebupookins.net>
2012-11-21 12:59:28 -0500

    NPCs now have labels identifying who they are.

--- app/components/NPC.js
+++ app/components/NPC.js
@@ -15,6 +15,21 @@ define([
 		NPC: function(heightMap, worldToPixel, pathFinder, initialX, initialY, properties) {
 			this.Character(heightMap, worldToPixel, pathFinder, initialX, initialY, properties);
 			utils.assert(properties.name, 'NPCs must have a name.');
+			var PIXEL_PER_CHAR = 24;
+			var estimatedLabelWidth = properties.name.length * PIXEL_PER_CHAR / 2.5; //2.5 was determined empirically
+			var labelX = this.x + ((this.w - estimatedLabelWidth)/ 2)
+			Crafty.e('Canvas, BetterText').attr({
+				text: properties.name,
+				fontSize: PIXEL_PER_CHAR+'px',
+				fontFamily: 'Patrick Hand',
+				fillStyle: 'white',
+				strokeStyle: 'black',
+				x: labelX,
+				y: this.y - 32,
+				z: config.zOffset.gitk - 1,
+				w: estimatedLabelWidth,
+				h: PIXEL_PER_CHAR,
+			});
 			return this;
 		}
 	});
