3c82b310c54801e3cb664705fc617e74e68c763e
antportal165 <antportal165@gmail.com>
2012-11-22 17:18:13 -0500

    Added 'click to continue' prompt to intro

--- app/scenes/intro.js
+++ app/scenes/intro.js
@@ -14,6 +14,15 @@ define([
 			fontSize: config.dialogFont.size,
 			text: "<p>In 1434, long before Charles Babbage had invented the programmable computer, a wise mage by the name of Linus Isopaa invented git, a powerful set of magic spells which allowed the people of his village to collaboratively create, edit, and safeguard all of their writings.</p> <p>For 6 years, they flourished and Linus gained a vast number of apprentices.</p> <p>However, the process of learning the magic words took time, and in 1440, Johannes Gutenberg invented the printing press.</p> <p>As a result, very few people, to this day, remember the wonders of using git magic...</p>"
 		});
+		Crafty.e('2D, DOM, BetterText').attr({
+			x: 0,
+			y: config.viewport.height - 40,
+			w: config.viewport.width,
+			h: 40,
+			fontFamily: config.dialogFont.family,
+			fontSize: config.dialogFont.size / 3,
+			text: "<p style='text-align: center'>Click to continue</p>"
+		});
 		Crafty.e('2D, Mouse').attr({
 			x: 0,
 			y: 0,
