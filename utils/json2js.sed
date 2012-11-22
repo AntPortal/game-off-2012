557647e70bba6b77dac8a6d7ad74547c215f09e6
antportal165 <antportal165@gmail.com>
2012-11-22 15:39:08 -0500

    Fixed json2js.sh so it doesn't pass '-f -' to sed

---
+++ utils/json2js.sed
@@ -0,0 +1,4 @@
+1 i\
+define(function(){return(
+$ a\
+);});
--- utils/json2js.sh
+++ utils/json2js.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 project_dir=$(dirname $0)/..
-sed -f - -i "${project_dir}/assets/maps/level1.json.js" <<-"EOF"
-1 i\
-define(function(){return(
-$ a\
-);});
-EOF
+sed -f "${project_dir}/utils/json2js.sed" -i "${project_dir}/assets/maps/level1.json.js"
