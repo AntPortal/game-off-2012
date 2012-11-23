db41857b004432eac3a9b5c3ff4c347b89a5a84c
antportal165 <antportal165@gmail.com>
2012-11-22 21:35:54 -0500

    Fixed font on intro screen

--- app/scenes/intro.js
+++ app/scenes/intro.js
@@ -11,7 +11,7 @@ define([
 			w: config.viewport.width - 100,
 			h: config.viewport.height - 100,
 			fontFamily: config.dialogFont.family,
-			fontSize: config.dialogFont.size,
+			fontSize: config.dialogFont.size + 'px',
 			text: "<p>In 1434, long before Charles Babbage had invented the programmable computer, a wise mage by the name of Linus Isopaa invented git, a powerful set of magic spells which allowed the people of his village to collaboratively create, edit, and safeguard all of their writings.</p> <p>For 6 years, they flourished and Linus gained a vast number of apprentices.</p> <p>However, the process of learning the magic words took time, and in 1440, Johannes Gutenberg invented the printing press.</p> <p>As a result, very few people, to this day, remember the wonders of using git magic...</p>"
 		});
 		Crafty.e('2D, DOM, BetterText').attr({
@@ -20,7 +20,7 @@ define([
 			w: config.viewport.width,
 			h: 40,
 			fontFamily: config.dialogFont.family,
-			fontSize: config.dialogFont.size / 3,
+			fontSize: Math.round(config.dialogFont.size * 0.75) + 'px',
 			text: "<p style='text-align: center'>Click (or Tap) to continue</p>"
 		});
 		Crafty.e('2D, Mouse').attr({
