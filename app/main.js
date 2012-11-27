35f673caf914e57c398d4c59e7fddb19defc6641
Nebu Pookins <nebu@nebupookins.net>
2012-11-27 12:39:06 -0500

    Added detection for iOS5 devices, but untested as I don't have an iOS 5 device on hand.

--- app/main.js
+++ app/main.js
@@ -13,6 +13,9 @@ define([
 	'trapBackspace',
 ], function(config) {
 	console.log('Game starting...');
+	if (navigator.userAgent.match(/OS 5(_\d)+ like Mac OS X/i)) {
+		alert("This game requires iOS 6, but you are currently runnign iOS 5. Please upgrade your firmware to play this game.");
+	}
 	Crafty.init(config.viewport.width, config.viewport.height);
 	// Start in loading scene
 	Crafty.scene('Loading');
